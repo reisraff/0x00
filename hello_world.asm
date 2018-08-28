aa11ad5706a1ac083fdf4538332732b6d80bb430
Rafael Reis <reisraff@gmail.com>
2018-08-28 15:57:20 -0300

    hello world

---
+++ hello_world.asm
@@ -0,0 +1,16 @@
+section .text
+    global _start
+
+_start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, hl
+    mov edx, hll
+    int 0x80
+    mov eax, 1
+    int 0x80
+
+
+section .data
+    hl db 'Hello World', 0xa
+    hll equ $ - hl
